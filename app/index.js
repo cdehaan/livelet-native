a51b3fac9704130d86a22ce84a2a37add78ff492
cdehaan <cdehaan@gmail.com>
2024-07-03 12:34:18 +0900

    added index.js for compatibility

---
+++ app/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import Login from './Login';
+import Menu from './Menu';
+import { UserProvider } from '../contexts/UserContext';
+
+export default function Index() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <UserProvider>      
+      <View style={{ flex: 1 }}>
+        {isLoggedIn ? (
+          <Menu onLogout={setIsLoggedIn} />
+        ) : (
+          <Login onLogin={setIsLoggedIn} />
+        )}
+      </View>
+    </UserProvider>
+  );
+}
